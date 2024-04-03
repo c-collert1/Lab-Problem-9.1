07af77bd80dee1788d865248e3f2fcaee137e66e
Caleb Collert <c-collert.1@onu.edu>
2024-04-03 09:47:30 -0400

    Every element in order of appearance in the array.

--- P9-1/P9-1.cpp
+++ P9-1/P9-1.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 using namespace std;
 
+
+int fourDigitRandNum();
+
 int main() {
 
+	srand(time(NULL));
+	int fourDigitNumbers[10];
 
+	// Generation of 10 four digit numbers
+	for (int i = 0; i < 10; ++i) {
+		fourDigitNumbers[i] = fourDigitRandNum();
+	}
+	
+	// Displaying all four digit numbers
+	cout << "All four digit Numbers:  ";
+	for (int i = 0; i < 10; i++) {
+		cout << fourDigitNumbers[i] << "  ";
+	}
+	cout << endl;
+}
+
+//code for generation of 10 four digit numbers
+int fourDigitRandNum() {
+	return (rand() % 9 + 1) * 1000 + (rand() % 10) * 100 + (rand() % 10) * 10 + (rand() % 10);
 }
